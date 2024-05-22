5070fff8f5acfa1c2b58d7921a66767b32add319
Peter <schmidt.multimedia@gmail.com>
2024-05-22 10:16:03 +0200

    01 / Objekte durch new erzeugen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,6 +4,10 @@ public class App {
     
     public static void main(String[] args) {
 
+        // zwei Objekte erzeugen
+        System.out.println(new Cat());
+        System.out.println(new Cat());
+
 
     }
 
